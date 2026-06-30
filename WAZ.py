07800afdadf885df7af169f17d54986453ab3b0a
DsY044 <dawid.sak24@gmail.com>
2026-06-30 19:45:44 +0200

    Update WAZ.py

--- WAZ.py
+++ WAZ.py
@@ -1,22 +1,62 @@
-#global variables for free use więc nie musisz pisać liczbowo tylko piszesz słówka
-x = 200
-y = 200
-speed = 5
+gracz = None
+obiekty = [] #Lista, w której będą przechowywane spadające obiekty
 
-#setup gry całego ekranu mam nadzieje że wiecie o co chodzi to takie intro nie wiem jak to wytłumaczyć
+class Gracz:
+    def __init__(self):
+        self.x = 200
+        self.y = 350
+        self.szer = 30
+        self.wys = 50
+        self.speed = 5
+        
+    def ruch(self):
+        if keyPressed:
+            if keyCode == LEFT:
+                self.x -= self.speed
+            elif keyCode == RIGHT:
+                self.x += self.speed
+
+    def pokaz(self):
+        fill(0, 255, 0)
+        rect(self.x, self.y, self.szer, self.wys)
+        
+class SpadajacyObiekt:
+    def __init__(self):
+        # Losowe miejsce pojawienia się nad ekranem
+        self.x = random(width)
+        self.y = random(-400, 0)
+        self.speed = random(2, 5)
+
+    def aktualizuj(self):
+        # Przesunięcie obiektu w dół
+        self.y += self.speed
+
+        # Jeśli obiekt spadnie poza ekran, wraca na górę w losowym miejscu
+        if self.y > height:
+            self.y = random(-100, 0)
+            self.x = random(width)
+
+    def pokaz(self):
+        fill(255, 0, 0)
+        ellipse(self.x, self.y, 20, 20)
 def setup():
+    global gracz
+
     size(400, 400)
-    
+
+    gracz = Gracz()
+
+    # Tworzy 10 spadających obiektów
+    for i in range(10):
+        obiekty.append(SpadajacyObiekt())
+
+
 def draw():
-    global x, y
-
     background(220)
-    
-#tu ruch
-    if keyPressed:
-        if keyCode == LEFT:
-            x -= speed
-        if keyCode == RIGHT:
-            x += speed
 
-    rect(x, 345, 50, 50)
+    gracz.ruch()
+    gracz.pokaz()
+
+    for o in obiekty:
+        o.aktualizuj()
+        o.pokaz()
